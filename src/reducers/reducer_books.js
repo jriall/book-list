ba5ba71f5cfc6c202211914cf529ab96e55a7408
JamesRiall <jamesrariall@gmail.com>
2017-10-21 16:14:59 +0100

    completed book list tutorial

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    { title: 'The Good Parts', pages: 101 },
+    { title: 'Harry Potter', pages: 85 },
+    { title: 'The Dark Tower', pages: 10201 },
+    { title: 'Eloquent Ruby', pages: 10 },
+  ];
+}
